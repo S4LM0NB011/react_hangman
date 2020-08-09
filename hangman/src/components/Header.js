c36d81e244f5797e90baffbe9ed1e2813faac2e2
Mattias Alexander Coelho <mattic15@student.uia.no>
2020-08-09 22:54:10 +0200

    added header component

---
+++ hangman/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1 className="title">Hangman for PogChamps</h1>
+            <p className="subtitle">Find the hidden word. You won't... PepeLaugh</p>
+        </div>
+    )
+}
+
+export default Header;
