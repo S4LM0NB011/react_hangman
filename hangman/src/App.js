676e7482e7e7277fed51182da94ae6888bcd0659
Mattias Alexander Coelho <mattic15@student.uia.no>
2020-08-11 14:54:28 +0200

    basics of game is functional, needs feedback, bigger word bank, twitch intergration

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Gallow from './components/Gallow';
+import Word from './components/Word';
+import WrongLetters from './components/WrongLetters';
+import './App.css';
+
+const words = ['wrong', 'right', 'nope', 'yep'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() =>{
+    const handleKeyDown = event => {
+      const { key, keyCode} = event;
+      if (playable && keyCode >= 65 && keyCode <= 90){
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if(!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [ ... currentLetters, letter]);
+          } else {
+            //Do something
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [ ... wrongLetters, letter]);
+          }
+        }
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+
+  }, [correctLetters, wrongLetters, playable]);
+
   return (
     <div className="App">
+      <Header />
+      <Gallow />
+      <WrongLetters wrongLetters={wrongLetters} />
+      <Word selectedWord={selectedWord} correctLetters={correctLetters} />
     </div>
   );
 }
--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Header from './components/Header';
-import Gallow from './components/Gallow'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Header />
-    <Gallow />
   </React.StrictMode>,
   document.getElementById('root')
 );
