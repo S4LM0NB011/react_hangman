7c5b6f1fc10853adede5255a15c5ec6a4c3351da
Mattias Alexander Coelho <mattic15@student.uia.no>
2020-08-11 13:24:10 +0200

    added gallow to components, changed styling to css

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './components/Header';
+import Gallow from './components/Gallow'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Header />
+    <Gallow />
   </React.StrictMode>,
   document.getElementById('root')
 );
